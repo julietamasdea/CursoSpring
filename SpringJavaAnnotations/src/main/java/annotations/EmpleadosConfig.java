ba4e16f6a396b2f7adc1e2d9422236ea4506fe30
Julieta Masdea <julieta.masdea@globallogic.com>
2020-11-28 16:27:35 -0300

    Java annotation @Configuration, reemplazo del archivo applicationContext.xml

---
+++ SpringJavaAnnotations/src/main/java/annotations/EmpleadosConfig.java
@@ -0,0 +1,9 @@
+package annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("annotation")
+public class EmpleadosConfig {
+}
--- SpringJavaAnnotations/src/main/java/annotations/UsoSingletonPrototype.java
+++ SpringJavaAnnotations/src/main/java/annotations/UsoSingletonPrototype.java
@@ -1,11 +1,16 @@
 package annotations;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UsoSingletonPrototype {
     public static void main(String[] args) {
         //leer xml de configuracion
-        ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //leer el class de configuracion
+        AnnotationConfigApplicationContext contexto = new AnnotationConfigApplicationContext(EmpleadosConfig.class);
+
         //pedir bean al contenedor
         IEmpleados antonio = contexto.getBean("ComercialExperimentado", IEmpleados.class);
         IEmpleados juan = contexto.getBean("ComercialExperimentado", IEmpleados.class);
