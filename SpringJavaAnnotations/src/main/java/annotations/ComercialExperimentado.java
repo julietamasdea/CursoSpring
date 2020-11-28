9ebbfd083863b2a3b6d49cdd58ef2263c72e620a
Julieta Masdea <julieta.masdea@globallogic.com>
2020-11-28 15:33:58 -0300

    Agrego teoria de annotations

--- SpringJavaAnnotations/src/main/java/annotations/ComercialExperimentado.java
+++ SpringJavaAnnotations/src/main/java/annotations/ComercialExperimentado.java
@@ -8,14 +8,12 @@ import org.springframework.stereotype.Component;
 //usoannotations pongo "comercialExperimentado" que es el nombre de la clase con la primera letra minuscula
 
 public class ComercialExperimentado implements IEmpleados{
-    @Autowired
     private Informe informe;
-    /*
+
     @Autowired //esto hace que spring busque en todo el proyecto si hay alguna clase que implemente la interfaz Informe
     public ComercialExperimentado(Informe informe) {
         this.informe = informe;
     }
-     */
 
     @Override
     public String getTareas() {
