3bde7a8d4837d98d5c89ccbac311c680bc2354c9
Julieta Masdea <julieta.masdea@globallogic.com>
2020-11-29 12:57:19 -0300

    @RequestParam para rescatar en un metodo los parametros del request

--- SpringWebApplications/src/main/java/web/AlumnoController.java
+++ SpringWebApplications/src/main/java/web/AlumnoController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,10 +23,10 @@ public class AlumnoController {
     }
 
     @RequestMapping("/procesaDatosFormularioAlumnos") //no necesariamente tiene que llamarse igual que el metodo
-    public String procesaDatosFormularioAlumnos(HttpServletRequest request, Model model) { //proporciona el formulario
-
-        //leer la informacion que viene del cuadro de texto
-        String nombre = request.getParameter("nombreAlumno");
+    //public String procesaDatosFormularioAlumnos(HttpServletRequest request, Model model) { //proporciona el formulario
+    public String procesaDatosFormularioAlumnos(@RequestParam("nombreAlumno") String nombre, Model model) {
+            //leer la informacion que viene del cuadro de texto
+        //String nombre = request.getParameter("nombreAlumno");
         nombre += " es el mejor alumno";
         String mensajeFinal = "¿Quien es el mejor alumno? " + nombre;
 
